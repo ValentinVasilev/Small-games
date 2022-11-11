32441fba8793e2219ffaf7b873f48a20c6136d3c
ValentinVasilev <falioVasilev@gmail.com>
2022-11-11 13:16:36 +0200

    Lottery player section initially creaed

--- small-games/pages/lottery.tsx
+++ small-games/pages/lottery.tsx
@@ -8,8 +8,10 @@ const Lottery = () => {
   const [currentJackpot, setCurrentJackpot] = useState<number>(777777)
   const [peoplePlayed, setPeoplePlayed] = useState<number>(0)
   const [playerWallet, setPlayerWallet] = useState<number>(100)
-  const [myNumbers, setMyNumbers] = useState<Array<number>>([2, 15, 7, 38, 42, 27])
+  const [myNumbers, setMyNumbers] = useState<Array<number>>([])
 
+
+  // Generate the wining numbers
   const generateNumbers = () => {
 
     let rotation = 6;
@@ -26,12 +28,14 @@ const Lottery = () => {
     }
   }
 
+  // Generate the wining pot
   const generateWiningPot = () => {
     setPeoplePlayed(Math.floor(Math.random() * 1000000))
-    setCurrentJackpot((prev: any) => prev + peoplePlayed)
+    setCurrentJackpot((prev: number) => prev + peoplePlayed)
   }
 
-  const generateWiningNumbers = () => {
+  // Generate the game
+  const generateGame = () => {
     generateWiningPot()
 
     if (winingNumbers.length > 0) {
@@ -43,20 +47,53 @@ const Lottery = () => {
     }
   }
 
+
+  const generatePlayerNumbers = () => {
+
+    let rotation = 6;
+    for (let index = 0; index < rotation; index++) {
+      const element = Math.floor(Math.random() * 49);
+
+      if (!winingNumbers.includes(element)) {
+        setMyNumbers((prev) => [...prev, element])
+      } else {
+        rotation += 1;
+        continue;
+      }
+    }
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Lottery 6/49</h1>
-      <h3>Jackpot: ${currentJackpot.toLocaleString()}</h3>
-      <h5>People Played: {peoplePlayed}</h5>
-      <div className={styles.winingContainer}>
-        {
-          winingNumbers.map((num: number, index: number) => {
-            return <WiningNumber key={index} winingNumber={num} pickedNumbers={myNumbers} />
-          })
-        }
+      <div>
+        <section>
+          <h4>Game Rules</h4>
+        </section>
+        <div>
+          <section className={styles.gameSection}>
+            <h3>Jackpot: ${currentJackpot.toLocaleString()}</h3>
+            <h5>People Played: {peoplePlayed.toLocaleString()}</h5>
+            <div className={styles.winingContainer}>
+              {
+                winingNumbers.map((num: number, index: number) => {
+                  return <WiningNumber key={index} winingNumber={num} pickedNumbers={myNumbers} />
+                })
+              }
+            </div>
+            <button className={styles.btn} onClick={() => generateGame()}>Generate Game</button>
+          </section>
+          <section className={styles.playerSection}>
+            <h3>Wallet: ${playerWallet}</h3>
+            {
+              myNumbers.map((num: number, index: number) => {
+                return <p key={index}>{num}</p>
+              })
+            }
+            <button className={styles.btn} onClick={() => generatePlayerNumbers()}>Generate my numbers</button>
+          </section>
+        </div>
       </div>
-
-      <button className={styles.btn} onClick={() => generateWiningNumbers()}>Generate Numbers</button>
     </div>
   )
 }
